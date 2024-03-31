06c768df451c587e2375e28ba6c6bbdcb6d2ee15
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-03-31 00:06:03 -0700

    Huge Initial commit

---
+++ models/campground.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String
+});
+
+export default mongoose.model('Campground', CampgroundSchema);
