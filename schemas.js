dc063426d1c1b2ff41163c6d0329c17e39703385
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-03-31 22:05:15 -0700

    Added joi schema for review to enable server side validation

--- schemas.js
+++ schemas.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-export const campgroundSchema = Joi.object({
+const campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
@@ -9,3 +9,12 @@ export const campgroundSchema = Joi.object({
         description: Joi.string().required()
     }).required()
 });
+
+const reviewSchema = Joi.object({
+    review: Joi.object({
+        body: Joi.string().required(),
+        rating: Joi.number().required().min(1)
+    }).required()
+});
+
+export { campgroundSchema, reviewSchema };
