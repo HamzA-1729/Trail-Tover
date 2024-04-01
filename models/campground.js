35ce8fb4da5ccc2bc94220a62ebb1deca0a0cba4
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-03-31 20:59:51 -0700

    Embeded the Review Schema into campground Schema

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const CampgroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 export default mongoose.model('Campground', CampgroundSchema);
