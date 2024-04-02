0f3da8930cfa314d43b10a975064befba4b40208
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-04-02 21:00:10 +0500

    New route error solved

--- routes/campground.js
+++ routes/campground.js
@@ -16,7 +16,7 @@ const validateCampground = (req, res, next) => {
     }
 }
 
-router.get('', catchAsync(async (req, res) => {
+router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 }));
@@ -26,10 +26,11 @@ router.get('/new', (req, res) => {
 })
 
 
-router.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash("success", "Campground Added Successfully!");
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
