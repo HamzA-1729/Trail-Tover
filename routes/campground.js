c6e40f0f4182bba79c0c5aa609f2cdd3abce06b8
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-04-03 19:48:49 +0500

    Basic Flash messages added!

--- routes/campground.js
+++ routes/campground.js
@@ -27,8 +27,11 @@ router.get('/new', (req, res) => {
 
 
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
+    if (!campground) {
+        req.flash("error", "OOps Campground not Created!");
+        res.redirect("/campgrounds");
+    }
     await campground.save();
     req.flash("success", "Campground Added Successfully!");
     res.redirect(`/campgrounds/${campground._id}`)
@@ -36,17 +39,26 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate("reviews");
+    if (!campground) {
+        req.flash("error", "OOps Campground not Found!");
+        res.redirect("/campgrounds");
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if (!campground) {
+        req.flash("error", "OOps Campground not Created!");
+        res.redirect("/campgrounds");
+    }
     res.render('campgrounds/edit', { campground });
 }))
 
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash("success", "Campground Updated Successfully!");
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
@@ -59,6 +71,7 @@ router.delete('/:id', catchAsync(async (req, res) => {
         }
     });
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "Campground Deleted Successfully!");
     res.redirect('/campgrounds');
 }));
 
