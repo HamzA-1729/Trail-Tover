e5ff9f978af052c494b385af73ddd016521f9861
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-04-09 14:01:28 +0500

    Routes for register and login added

---
+++ routes/user.js
@@ -0,0 +1,34 @@
+import express from "express";
+import User from "../models/user.js";
+import passport from "passport";
+import catchAsync from "../utils/catchAsync.js";
+const router = express.Router();
+
+router.get("/register", (req, res) => {
+    res.render("user/register");
+});
+
+router.get("/login", (req, res) => {
+    res.render("user/login");
+});
+
+router.post("/register", catchAsync(async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const newUser = new User({ username, email });
+        await User.register(newUser, password);
+        req.flash("success", "Register successfully");
+        res.redirect("/campgrounds");
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/register");
+    }
+
+}));
+
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+    req.flash("success", "Logged in successfully");
+    res.redirect("/campgrounds");
+});
+
+export default router;
