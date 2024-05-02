e16b4d3c6150397deda1116232935a5fb22a9604
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-05-02 17:48:08 +0500

    Reviews author populated changes made in show get route

--- routes/campground.js
+++ routes/campground.js
@@ -28,9 +28,9 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.get('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id', catchAsync(async (req, res) => {
     try {
-        const campground = await Campground.findById(req.params.id).populate("reviews").populate("author");
+        const campground = await Campground.findById(req.params.id).populate({ path: "reviews", populate: { path: "author" } }).populate("author");
         if (!campground) {
             req.flash("error", "Oops! Campground not found.");
             return res.redirect("/campgrounds");
