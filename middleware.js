ad15a82c55d13c6334406f4ddb571cc0e9dbf320
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-05-02 11:16:17 +0500

    Middlewares from campground and review moved here

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+import { campgroundSchema } from "./schemas.js";
+import Campground from "./models/campground.js";
+import ExpressError from "./utils/ExpressError.js";
+import { reviewSchema } from "./schemas.js";
 const isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.flash("error", "You must be Logged In");
@@ -6,4 +10,33 @@ const isLoggedIn = (req, res, next) => {
     next();
 };
 
-export default isLoggedIn;
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+const isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash("error", "OOps u dont have the permission to access!");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+export { isLoggedIn, validateCampground, isAuthor, validateReview };
